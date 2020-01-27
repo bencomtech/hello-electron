75f7f5e918ac9fc920fa72236ffda390888da3e7
pichid <pichid.de@hengleasing.co.th>
2020-01-27 19:48:13 +0700

    Add files

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "/../public/index.html"));
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
